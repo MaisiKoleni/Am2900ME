a86ee0a975446429d3cc29cf6f507c6883b50a20
MaisiKoleni <maisi.koleni@t-online.de>
2019-01-08 23:56:13 +0100

    Fixed issue #1

--- src/main/java/net/maisikoleni/am2900me/logic/microinstr/RB_ADDR.java
+++ src/main/java/net/maisikoleni/am2900me/logic/microinstr/RB_ADDR.java
@@ -3,10 +3,10 @@ package net.maisikoleni.am2900me.logic.microinstr;
 import net.maisikoleni.am2900me.util.NBitsUInt;
 
 public class RB_ADDR extends NBitsUInt implements µIField {
-	static final RB_ADDR DEFAULT = new RB_ADDR(0x00);
+	static final RB_ADDR DEFAULT = new RB_ADDR(0x0);
 
-	public RB_ADDR(int uint_8bit) {
-		super(8, uint_8bit);
+	public RB_ADDR(int uint_4bit) {
+		super(4, uint_4bit);
 	}
 
 	@Override
