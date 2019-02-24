de315b4e0290791645c4b3e6e85f31b8f04158cf
MaisiKoleni <maisi.koleni@t-online.de>
2019-02-24 17:36:45 +0100

    Fixed issue that reset did not reset the RAM properly

--- src/main/java/net/maisikoleni/am2900me/logic/Am2900Machine.java
+++ src/main/java/net/maisikoleni/am2900me/logic/Am2900Machine.java
@@ -178,6 +178,7 @@ public class Am2900Machine {
 		mi = MicroInstruction.DEFAULT.withAm2910_Inst(Am2910_Inst.JZ);
 		pc.reset();
 		ir.reset();
+		machineRam.reset();
 		am2904_01x4.reset();
 		am2910.reset();
 	}
