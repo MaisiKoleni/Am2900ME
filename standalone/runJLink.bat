548c881d1fe1724a2137f4f83479c8b09fd8bb70
Christian Femers <femers@in.tum.de>
2020-02-11 01:26:37 +0100

    Switch to Java 13 and release version 1.0.0

--- standalone/runJLink.bat
+++ standalone/runJLink.bat
@@ -1,6 +1,6 @@
 REM Set the Version that is present in ./target/ and then jlink uses the jmods of the folders jmods_..., where the JDK jmods AND OpenJavaFX jmods are located. JAVA_HOME must be set.
-SET VERSION=0.1.1
+SET VERSION=1.0.0
 SET OPTIONS= --add-modules am2900me --launcher am2900me=am2900me/net.maisikoleni.am2900me.ui.Main --strip-debug --compress 2 --no-header-files --no-man-pages 
-"%JAVA_HOME%/bin/jlink" %OPTIONS% -p jmods_jdk11+ojfx_win;../target/Am2900ME-%VERSION%.jar --output distribution/am2900me_%VERSION%_win
-"%JAVA_HOME%/bin/jlink" %OPTIONS% -p jmods_jdk11+ojfx_linux;../target/Am2900ME-%VERSION%.jar --output distribution/am2900me_%VERSION%_linux
-"%JAVA_HOME%/bin/jlink" %OPTIONS% -p jmods_jdk11+ojfx_mac;../target/Am2900ME-%VERSION%.jar --output distribution/am2900me_%VERSION%_mac
+"%JAVA_HOME%/bin/jlink" %OPTIONS% -p jmods_jdk13+ojfx_win;../target/Am2900ME-%VERSION%.jar --output distribution/am2900me_%VERSION%_win
+"%JAVA_HOME%/bin/jlink" %OPTIONS% -p jmods_jdk13+ojfx_linux;../target/Am2900ME-%VERSION%.jar --output distribution/am2900me_%VERSION%_linux
+"%JAVA_HOME%/bin/jlink" %OPTIONS% -p jmods_jdk13+ojfx_mac;../target/Am2900ME-%VERSION%.jar --output distribution/am2900me_%VERSION%_mac
